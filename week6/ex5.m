5581d5a5cfc24e9ecc0abc92327ddeeed57ea724
Adrian Marino <adrianmarino@gmail.com>
2019-11-03 13:10:39 -0300

    use polinomial regresion to compute test set error

--- week6/ex5.m
+++ week6/ex5.m
@@ -226,13 +226,23 @@ fprintf('Program paused. Press enter to continue.\n');
 #
 #
 # --------------------------------------------------------------------------------------
-# Extra: Plot error over test set
+# Extra: Get test error for best lambda over cross validation set
 # --------------------------------------------------------------------------------------
-[min_val_error, min_val_error_lambda] = min(error_val, [], 1);
+[min_val_error, min_val_error_idx] = min(error_val, [], 1);
+min_val_error_lambda = lambda_vec(min_val_error_idx)
 fprintf('\n\nBest lambda hiper-parameter value: %d (Val error: %f).\n', min_val_error_lambda, min_val_error);
 
 # Train with best lambda value...
-best_theta = trainLinearReg([ones(m, 1) X], y, lambda);
-test_m = size(Xtest, 1);
-test_error = linearRegCostFunction([ones(test_m, 1) Xtest], ytest, best_theta, 0);
-test_error
+[theta] = trainLinearReg(X_poly, y, min_val_error_lambda);
+test_error = linearRegCostFunction(X_poly_test, ytest, theta, 0);
+fprintf('Test error with lambda %d using polinomila regresion: %f.\n', min_val_error_lambda, test_error);
+#
+#
+#
+#
+#
+#
+#
+# --------------------------------------------------------------------------------------
+# Extra: 
+# --------------------------------------------------------------------------------------
