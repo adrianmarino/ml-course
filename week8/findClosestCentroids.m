8a6ea4dc436ed6b557c406c9e26c6e2efe0813ac
Adrian Marino <adrianmarino@gmail.com>
2019-12-15 18:51:40 -0300

    first k-mean exercice working

--- week8/findClosestCentroids.m
+++ week8/findClosestCentroids.m
@@ -4,10 +4,12 @@ function idx = findClosestCentroids(X, centroids)
     idx = zeros(M, 1);
 
     for x_idx=1:M
-        x_row = X(x_idx, :)
-        x_row_rep = repmat(x_row, K, 1);
-        dists = vecnorm(x_row_rep - centroids, 2, 2).^2;
+        x = X(x_idx, :);
+         
+        x_rep = repmat(x, K, 1);
+        dists = vecnorm(x_rep - centroids, 2, 2).^2;
         [_, min_dist_k] = min(dists, [], 1);
+
         idx(x_idx) = min_dist_k;
     end
 end
