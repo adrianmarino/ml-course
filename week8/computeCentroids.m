04672579ded2c6f14d61caa80d69bcc5e6cf79ed
Adrian Marino <adrianmarino@gmail.com>
2019-12-15 19:00:29 -0300

    best code explanation

--- week8/computeCentroids.m
+++ week8/computeCentroids.m
@@ -1,8 +1,16 @@
 function centroids = computeCentroids(X, idx, K)
     centroids = zeros(K, size(X, 2));
     for k=1:K
-        x_k = X(idx == k, :);
+        # Get position for all elements equals to k
+        k_indexes = idx == k;
+
+        # Get x_rows with k_indexes... 
+        x_k = X(k_indexes, :);
+
+        # x_k count...
         c_k = size(x_k, 1);
+
+        # Calculate next k centroid position... 
         centroids(k, :) = sum(x_k) / c_k;
     end
 end
--- week8/findClosestCentroids.m
+++ week8/findClosestCentroids.m
@@ -3,11 +3,17 @@ function idx = findClosestCentroids(X, centroids)
     M = size(X, 1);
     idx = zeros(M, 1);
 
+    # For each X row ((x,y) point)...
     for x_idx=1:M
-        x = X(x_idx, :);
-         
-        x_rep = repmat(x, K, 1);
+        x_row = X(x_idx, :);
+ 
+        # Get matrix with "k" x_rows(repeated)...
+        x_rep = repmat(x_row, K, 1);
+
+        # Distance bethween x and each centroid...
         dists = vecnorm(x_rep - centroids, 2, 2).^2;
+ 
+        # Get centroid "k" with min distance to x...
         [_, min_dist_k] = min(dists, [], 1);
 
         idx(x_idx) = min_dist_k;
