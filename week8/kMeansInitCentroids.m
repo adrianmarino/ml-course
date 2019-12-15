dfb0e3956ff7f853a2521f374faba3ae1e4700e2
Adrian Marino <adrianmarino@gmail.com>
2019-12-15 19:12:19 -0300

    random initialization: select centroids randomly

--- week8/kMeansInitCentroids.m
+++ week8/kMeansInitCentroids.m
@@ -1,26 +1,14 @@
 function centroids = kMeansInitCentroids(X, K)
-%KMEANSINITCENTROIDS This function initializes K centroids that are to be 
-%used in K-Means on the dataset X
-%   centroids = KMEANSINITCENTROIDS(X, K) returns K initial centroids to be
-%   used with the K-Means on the dataset X
-%
+    # Number of examples...
+    m = size(X, 1);
 
-% You should return this values correctly
-centroids = zeros(K, size(X, 2));
+    # Get array with m random values bethween "1..m"... 
+    random_indexes = randperm(m);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: You should set centroids to randomly chosen examples from
-%               the dataset X
-%
+    # Get first K indexes...
+    centroid_indexes = random_indexes(1:K);
 
-
-
-
-
-
-
-
-% =============================================================
-
+    # Get centroids by index...
+    centroids = X(centroid_indexes, :);
 end
 
