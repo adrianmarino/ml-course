59f274add460af63cb610fbbeb2d472ce046d9e7
Adrian Marino <adrianmarino@gmail.com>
2020-01-18 19:58:56 -0300

    week 9 finished

--- week9/cofiCostFunc.m
+++ week9/cofiCostFunc.m
@@ -1,5 +1,6 @@
 function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
                                   num_features, lambda)
+
     % Y: num_movies x num_users matrix of user ratings of movies.
     % R: num_movies x num_users matrix, where R(i, j) = 1 if the 
     %    i-th movie was rated by the j-th user.
@@ -11,31 +12,27 @@ function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
     Theta = reshape(params(num_movies*num_features+1:end), num_users, num_features); % (4x3)
 
     % Calculate J Cost
+
+    % Error value for each movie vs. user. ERROR(i,j).
     %      (5x3)  (3x4)  (5x4)  (5x4)    
     Error = (X  * Theta' - Y)  .* R; % (5x4)
-    J = 1/2 * sum(sum(Error.^2));
 
-    X_grad = zeros(size(X));
-    Theta_grad = zeros(size(Theta));
+    j_reg = lambda/2 * (sum(sum(Theta.^2)) + sum(sum(X.^2)));
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Compute the cost function and gradient for collaborative
-    %               filtering. Concretely, you should first implement the cost
-    %               function (without regularization) and make sure it is
-    %               matches our costs. After that, you should implement the 
-    %               gradient and use the checkCostFunction routine to check
-    %               that the gradient is correct. Finally, you should implement
-    %               regularization
-    %
-    % You should set the following variables correctly:
-    %
-    %        X_grad - num_movies x num_features matrix, containing the 
-    %                 partial derivatives w.r.t. to each element of X
-    %        Theta_grad - num_users x num_features matrix, containing the 
-    %                     partial derivatives w.r.t. to each element of Theta
-    %
+    J = 1/2 * sum(sum(Error.^2)) + j_reg;
 
-    % =============================================================
+    % X_grad: num_movies x num_features matrix, containing the partial derivatives w.r.t. to each element of X.
+    % Theta_grad: num_users x num_features matrix, containing the partial derivatives w.r.t. to each element of Theta.
+
+    X_grad_reg = lambda * X
+
+    %         (5x4)   (4x3)
+    X_grad = Error * Theta + X_grad_reg; % (5x3)
+
+    Theta_gra_reg = lambda * Theta;
+
+    %           (4x5)   (5x3)
+    Theta_grad = Error' * X + Theta_gra_reg; % (4x3)
 
     grad = [X_grad(:); Theta_grad(:)];
 end
